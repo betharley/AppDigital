d7c9bbdd53bb44d861ddc1784286767dcd305fc4
betharley <betharley@gmail.com>
2020-10-02 22:29:22 -0300

    Adicionado Login Activity

--- App04_SetOnClickListener/app/src/main/java/com/betharley/setonclicklistener/MainActivity.kt
+++ App04_SetOnClickListener/app/src/main/java/com/betharley/setonclicklistener/MainActivity.kt
@@ -1,20 +1,11 @@
-package com.betharley.loginactivity
+package com.betharley.setonclicklistener
 
-import android.os.Bundle
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-
     }
 }
